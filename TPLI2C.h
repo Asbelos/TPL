53c259b1268525d9fb9a17446bcb658fbe748968
Asbelos <asbelos@btinternet.com>
2020-06-24 11:06:28 +0100

    No longer required

--- TPLI2C.h
+++
@@ -1,7 +0,0 @@
-class TPLI2C {
- public:
-   static void begin();
-   static void write(uint8_t i2caddr,byte buffer[], byte size);
-   static void  writeRegister(byte deviceAddr,byte reg, byte value);
-   static uint16_t  read2Registers(byte deviceAddr,byte reg);
-};
